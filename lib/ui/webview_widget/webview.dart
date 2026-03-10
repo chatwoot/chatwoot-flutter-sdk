d09104f9820dcf894d0b9d5480db68087763996b
Ephraim Nartey <enartey@adiutor.co>
2026-03-10 10:13:00 +0000

    chore: replace webview_flutter with flutter_inappwebview package

--- lib/ui/webview_widget/webview.dart
+++ lib/ui/webview_widget/webview.dart
@@ -1,13 +1,12 @@
+import 'dart:collection';
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:chatwoot_sdk/chatwoot_sdk.dart';
 import 'package:chatwoot_sdk/ui/webview_widget/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_flutter_android/webview_flutter_android.dart'
-    as webview_flutter_android;
+
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 ///Chatwoot webview widget
 /// {@category FlutterClientSdk}
@@ -20,9 +19,6 @@ class Webview extends StatefulWidget {
 
   /// See [ChatwootWidget.closeWidget]
   final void Function()? closeWidget;
-
-  /// See [ChatwootWidget.onAttachFile]
-  final Future<List<String>> Function()? onAttachFile;
 
   /// See [ChatwootWidget.onLoadStarted]
   final void Function()? onLoadStarted;
@@ -41,7 +37,6 @@ class Webview extends StatefulWidget {
       String locale = "en",
       customAttributes,
       this.closeWidget,
-      this.onAttachFile,
       this.onLoadStarted,
       this.onLoadProgress,
       this.onLoadCompleted})
@@ -58,74 +53,109 @@ class Webview extends StatefulWidget {
 }
 
 class _WebviewState extends State<Webview> {
-  WebViewController? _controller;
+  InAppWebViewController? _controller;
+  String? _webviewUrl;
+
   @override
   void initState() {
     super.initState();
+
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       String webviewUrl = widget.widgetUrl;
+
       final cwCookie = await StoreHelper.getCookie();
       if (cwCookie.isNotEmpty) {
-        webviewUrl = "${webviewUrl}&cw_conversation=${cwCookie}";
+        webviewUrl = "$webviewUrl&cw_conversation=$cwCookie";
       }
-      setState(() {
-        _controller = WebViewController()
-          ..setJavaScriptMode(JavaScriptMode.unrestricted)
-          ..setBackgroundColor(Colors.white)
-          ..setNavigationDelegate(
-            NavigationDelegate(
-              onProgress: (int progress) {
-                // Update loading bar.
-                widget.onLoadProgress?.call(progress);
-              },
-              onPageStarted: (String url) {
-                widget.onLoadStarted?.call();
-              },
-              onPageFinished: (String url) async {
-                widget.onLoadCompleted?.call();
-              },
-              onWebResourceError: (WebResourceError error) {},
-              onNavigationRequest: (NavigationRequest request) {
-                _goToUrl(request.url);
-                return NavigationDecision.prevent;
-              },
-            ),
-          )
-          ..addJavaScriptChannel("ReactNativeWebView",
-              onMessageReceived: (JavaScriptMessage jsMessage) {
-            print("Chatwoot message received: ${jsMessage.message}");
-            final message = getMessage(jsMessage.message);
-            if (isJsonString(message)) {
-              final parsedMessage = jsonDecode(message);
-              final eventType = parsedMessage["event"];
-              final type = parsedMessage["type"];
-              if (eventType == 'loaded') {
-                final authToken = parsedMessage["config"]["authToken"];
-                StoreHelper.storeCookie(authToken);
-                _controller?.runJavaScript(widget.injectedJavaScript);
-              }
-              if (type == 'close-widget') {
-                widget.closeWidget?.call();
-              }
-            }
-          })
-          ..loadRequest(Uri.parse(webviewUrl));
 
-        if (Platform.isAndroid && widget.onAttachFile != null) {
-          final androidController = _controller!.platform
-              as webview_flutter_android.AndroidWebViewController;
-          androidController
-              .setOnShowFileSelector((_) => widget.onAttachFile!.call());
-        }
+      setState(() {
+        _webviewUrl = webviewUrl;
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return _controller != null
-        ? WebViewWidget(controller: _controller!)
-        : SizedBox();
+    if (_webviewUrl == null) {
+      return const SizedBox();
+    }
+
+    return InAppWebView(
+      initialUrlRequest: URLRequest(
+        url: WebUri(_webviewUrl!),
+      ),
+      initialSettings: InAppWebViewSettings(
+        javaScriptEnabled: true,
+        allowFileAccessFromFileURLs: true,
+        allowUniversalAccessFromFileURLs: true,
+        transparentBackground: false,
+        allowFileAccess: true,
+        allowContentAccess: true,
+        mixedContentMode: MixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
+      ),
+      initialUserScripts: UnmodifiableListView([
+        UserScript(
+          source: """
+window.ReactNativeWebView = {
+  postMessage: function(message) {
+    window.flutter_inappwebview.callHandler('ReactNativeWebView', message);
+  }
+};
+window.postMessage = window.ReactNativeWebView.postMessage
+      """,
+          injectionTime: UserScriptInjectionTime.AT_DOCUMENT_START,
+        )
+      ]),
+      onWebViewCreated: (controller) async {
+        _controller = controller;
+        controller.addJavaScriptHandler(
+          handlerName: "ReactNativeWebView",
+          callback: (args) async {
+            if (args.isEmpty) return;
+
+            final message = getMessage(args.first);
+
+            if (isJsonString(message)) {
+              final parsedMessage = jsonDecode(message);
+
+              final eventType = parsedMessage["event"];
+              final type = parsedMessage["type"];
+
+              if (eventType == 'loaded') {
+                final authToken = parsedMessage["config"]["authToken"];
+                StoreHelper.storeCookie(authToken);
+
+                await controller.evaluateJavascript(
+                  source: widget.injectedJavaScript,
+                );
+              }
+
+              if (type == 'close-widget') {
+                widget.closeWidget?.call();
+              }
+            }
+          },
+        );
+
+      },
+      onProgressChanged: (controller, progress) {
+        widget.onLoadProgress?.call(progress);
+      },
+      onLoadStart: (controller, url) {
+        widget.onLoadStarted?.call();
+      },
+      onLoadStop: (controller, url) async {
+        widget.onLoadCompleted?.call();
+      },
+      shouldOverrideUrlLoading: (controller, navigationAction) async {
+        final uri = navigationAction.request.url;
+        if (uri != null) {
+          _goToUrl(uri.toString());
+        }
+        return NavigationActionPolicy.CANCEL;
+      },
+      onReceivedError: (controller, request, error) {},
+    );
   }
 
   _goToUrl(String url) {
